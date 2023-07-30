65e0c1685a67445c1a469d8912843179d1fdf09c
SANG\phamq <phamquangsang661@gmail.com>
2023-07-31 00:50:57 +0700

    feat: auto re init rabit mq when error

--- back-end/src/init-service.ts
+++ back-end/src/init-service.ts
@@ -6,14 +6,32 @@ import Logger from "@utils/logger";
 export default async () => {
   try {
     prisma.getInstance();
+  } catch (err) {
+    Logger.error("INIT", err);
+  }
 
+  const initRabbitMQSender = async () => {
     await rabbitMQSender.getInstance();
     await rabbitMQSender.signQueue(
       QUEUE.VIDEO_SERVICE.VOTE,
       QUEUE.NOTIFICATION_SERVICE.SHARING
     );
+  };
+  try {
+    // Init Rabbit MQ first time
+    await initRabbitMQSender();
   } catch (err) {
     Logger.error("INIT", err);
+    // Try to re-connect each 60 seconds
+    const reConnectMQInterval = setInterval(async () => {
+      try {
+        Logger.warn("RE-CONNECT RABBIT MQ", "Trying to re-connect");
+        await initRabbitMQSender();
+        clearInterval(reConnectMQInterval);
+      } catch (err) {
+        Logger.error("RE-CONNECT RABBIT MQ", err);
+      }
+    }, 60 * 1000);
   }
 
   return async () => {
