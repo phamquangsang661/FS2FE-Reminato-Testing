550a7b523a412ff8cc9a259bb20504f7567a2cb6
SANG\phamq <phamquangsang661@gmail.com>
2023-08-01 14:32:54 +0700

    feat(cypress): clean

--- front-end/cypress.config.ts
+++ front-end/cypress.config.ts
@@ -6,7 +6,5 @@ export default defineConfig({
       framework: "react",
       bundler: "vite",
     },
-    // JS-dom and vitest will not work with together
-    specPattern: ["__tests__/cypress/e2e/**/*.cy.{js,jsx,ts,tsx}"]
   },
 });
