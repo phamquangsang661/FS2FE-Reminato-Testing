ecd4410f30ea842d58198475dfc6c9b2171f868e
SANG\phamq <phamquangsang661@gmail.com>
2023-08-01 14:21:46 +0700

    feat(test): back to top component

--- front-end/__mocks__/fake/test-component.tsx
+++ front-end/__mocks__/fake/test-component.tsx
@@ -1,10 +1,21 @@
-import { forwardRef } from "react"
+import { ReactNode, forwardRef } from "react"
+
+
 
 export function TestComponent() {
     return <div>TESTING COMPONENT</div>
 }
 
-export const TestComponentForwardRef = forwardRef<HTMLDivElement>((_props, ref ) => {
+export interface TestChildComponent {
+    children?: ReactNode;
+    className?: string;
+}
+export function TestChildComponent({ children, className = "" }: TestChildComponent) {
+    return <div className={className}>{children}</div>
+}
+
+
+export const TestComponentForwardRef = forwardRef<HTMLDivElement>((_props, ref) => {
     {
         return <div ref={ref}>TESTING COMPONENT</div>
     }
--- front-end/tailwind.config.js
+++ front-end/tailwind.config.js
@@ -3,6 +3,7 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "./__mocks__/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {
